0729eea1c22f621867cf23f665deb258793bf2b8
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-07 21:27:55 -0500

    added Midje to the project

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject utilize "0.1.0"
   :description "Library of utility functions"
+  :dev-dependencies [[midje "1.2-alpha4"]]
   :dependencies [[clojure "1.2.0"]
                  [org.clojure/tools.macro "0.1.1"]])
