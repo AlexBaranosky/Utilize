64b2b1289a8d6a09bd84df74920a829e4516be20
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-07 22:24:23 -0500

    added ordinalize, ordinal-to-int and but-last-str

--- src/utilize/string.clj
+++ src/utilize/string.clj
@@ -31,3 +31,21 @@
      (plural 9 \"radius\" \"radii\") => \"9 radii\""
   [num singular & [plural]]
   (str num " " (if (= 1 num) singular (or plural (str singular "s")))))
+
+(defn but-last-str [s n]
+   (if (> n (.length s))
+       ""
+      (.substring s 0 (- (.length s) n))))
+
+(defn ordinal-to-int [ord]
+  (let [digits (but-last-str ord 2)]
+    (Integer/parseInt digits)))
+
+(defn ordinalize [int]
+  (if (contains? #{11 12 13} (mod int 100))
+    (str int "th")
+    (case (mod int 10)
+      1 (str int "st")
+      2 (str int "nd")
+      3 (str int "rd")
+      (str int "th"))))
--- test/utilize/string_test.clj
+++ test/utilize/string_test.clj
@@ -1,5 +1,5 @@
 (ns utilize.string-test
-  (:use utilize.string clojure.test))
+  (:use utilize.string clojure.test midje.sweet))
 
 (deftest to-camel
   (are [in out] (= out (camelize in))
@@ -29,3 +29,96 @@
   (is (= "1 cat" (pluralize 1 "cat")))
   (is (= "0 octopodes" (pluralize 0 "octopus" "octopodes")))
   (is (= "1 fish" (pluralize 1 "fish" "fishes"))))
+
+(tabular
+  (fact "cop off the last 'n' chars of a given string"
+    (but-last-str "1234" ?amt) => ?result)
+
+	?amt ?result
+	0    "1234"
+	1    "123"
+	2    "12"
+	3    "1"
+	4    ""
+	5    "")
+
+(tabular
+  (fact "convert ordinal strings to ints"
+    (ordinal-to-int ?ord) => ?int)
+
+	?ord  ?int
+	"1st"  1
+    "2nD"  2
+    "3RD"  3
+    "4Th"  4
+    "5th"  5
+    "6th"  6
+    "7th"  7
+    "8th"  8
+    "9th"  9
+    "10th" 10
+    "11th" 11
+    "12th" 12
+    "13th" 13
+    "14th" 14
+    "15th" 15
+    "16th" 16
+    "17th" 17
+    "18th" 18
+    "19th" 19
+    "20th" 20
+    "21st" 21
+    "22nd" 22
+    "23rd" 23
+    "24th" 24
+    "25th" 25
+    "26th" 26
+    "27th" 27
+    "28th" 28
+    "29th" 29
+    "30th" 30
+    "31st" 31
+    "331st" 331
+    "33331st" 33331)
+
+(tabular
+  (fact "convert int to a ordinal"
+    (ordinalize ?int) => ?ord)
+
+	?int ?ord
+    1  "1st"
+    2  "2nd"
+    3  "3rd"
+    4  "4th"
+    5  "5th"
+    6  "6th"
+    7  "7th"
+    8  "8th"
+    9  "9th"
+    10 "10th"
+    11 "11th"
+    12 "12th"
+    13 "13th"
+    14 "14th"
+    15 "15th"
+    16 "16th"
+    17 "17th"
+    18 "18th"
+    19 "19th"
+    20 "20th"
+    21 "21st"
+    22 "22nd"
+    23 "23rd"
+    24 "24th"
+    25 "25th"
+    26 "26th"
+    27 "27th"
+    28 "28th"
+    29 "29th"
+    30 "30th"
+    31 "31st"
+    311 "311th"
+    312 "312th"
+    313 "313th"
+    33331 "33331st")
+
