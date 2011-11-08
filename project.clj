68eb9f5af9defc098a4a2a2b429db68d38ea2290
Alex Baranosky <alexander.baranosky@gmail.com>
2011-11-07 23:24:16 -0500

    updated project file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject utilize "0.1.0"
-  :description "Library of utility functions"
+  :description "Compilation of Clojure functions from around the community"
   :dev-dependencies [[midje "1.2-alpha4"]]
   :dependencies [[clojure "1.2.0"]
                  [org.clojure/tools.macro "0.1.1"]
