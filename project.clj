2abc20e2b0823ff65cc2593678eb33a752fd142f
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-19 21:56:19 -0500

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject utilize "0.2.1"
   :description "Compilation of Clojure functions from around the community"
+  :url "https://github.com/AlexBaranosky/Utilize"
   :dev-dependencies [[midje "1.3.0"]]
   :dependencies [[clojure "1.3.0"]
                  [org.clojure/tools.macro "0.1.1"]
