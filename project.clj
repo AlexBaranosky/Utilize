58f3b015d8dda9e42976edf7a968912e94aa0cbf
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-19 21:51:42 -0500

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utilize "0.2.0"
+(defproject utilize "0.2.1"
   :description "Compilation of Clojure functions from around the community"
   :dev-dependencies [[midje "1.3.0"]]
   :dependencies [[clojure "1.3.0"]
